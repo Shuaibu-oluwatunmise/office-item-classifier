60c333f5381aa65acfddb26f322f587955103a8c
Shuaibu Oluwatunmise <shuaibuoluwatunmise@gmail.com>
2025-10-22 00:41:36 +0100

    inference.py adjusted to take in 9 classes

--- src/inference.py
+++ src/inference.py
@@ -31,7 +31,7 @@ def get_transform():
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-def load_model(num_classes=11):
+def load_model(num_classes=9):
     """
     Load trained model
     """
